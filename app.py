bdbb6a66e20760136840774ae80c50b8b053506a
youssef-elkahlaoui <youssefelkahlaoui18@gmail.com>
2024-12-15 08:33:30 +0100

    great project done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask, request, jsonify, render_template
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes, OperationStatusCodes
 from msrest.authentication import CognitiveServicesCredentials
